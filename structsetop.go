af2a879dc3bcfdd4551dc6543aea1d2859800c54
vtotiv <vito@dierksen.me>
2023-01-28 23:31:58 +0100

    implemented Intersection method for arbitrary structs

---
+++ structsetop.go
@@ -0,0 +1,47 @@
+package structsetop
+
+// package main
+
+func Union() {
+
+}
+
+// Intersection intersects A with B by comparing each element with the given compare func
+func Intersection[T any, U any, F ~func(*T, *U) bool](
+	A []T,
+	B []U,
+	equal F,
+) []T {
+	result := []T{}
+
+	if len(A) == 0 || len(B) == 0 {
+		return result
+	}
+
+	for _, a := range A {
+		for _, b := range B {
+			if equal(&a, &b) { 
+				result = append(result, a)
+				break
+			}
+		}
+	}
+
+	return result
+}
+
+func Difference() {
+
+}
+
+func SymmetricDifference() {
+
+}
+
+func Subset() {
+
+}
+
+func Superset() {
+
+}
---
+++ structsetop_test.go
@@ -0,0 +1,62 @@
+package structsetop
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// A few structs to test on
+type A struct {
+	va int
+}
+type B struct {
+	vb int
+}
+type C struct {
+	vc int
+}
+
+func TestUnion(t *testing.T) {
+
+}
+
+func TestIntersection(t *testing.T) {
+	arrOfA := []A{{1}, {2}, {3}}
+	arrOfB := []B{{2}, {3}, {4}}
+	arrOfC := []C{{4}, {5}}
+
+	expectIntersectionAB := []A{{2}, {3}}
+	expectIntersectionAC := []A{}
+	expectIntersectionBC := []B{{4}}
+
+	equalAB := func(a *A, b *B) bool {
+		return a.va == b.vb
+	}
+	equalAC := func(a *A, c *C) bool {
+		return a.va == c.vc
+	}
+	equalBC := func(b *B, c *C) bool {
+		return b.vb == c.vc
+	}
+
+	assert.Equal(t, expectIntersectionAB, Intersection(arrOfA, arrOfB, equalAB))
+	assert.Equal(t, expectIntersectionAC, Intersection(arrOfA, arrOfC, equalAC))
+	assert.Equal(t, expectIntersectionBC, Intersection(arrOfB, arrOfC, equalBC))
+}
+
+func TestDifference(t *testing.T) {
+
+}
+
+func TestSymmetricDifference(t *testing.T) {
+
+}
+
+func TestSubset(t *testing.T) {
+
+}
+
+func TestSuperset(t *testing.T) {
+
+}
