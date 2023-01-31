e5f774996402b188e15766cb595b4a850cbe2cf0
vtotiv <vito@dierksen.me>
2023-01-31 18:42:53 +0100

    Symmetric Difference Tests

--- structsetop_test.go
+++ structsetop_test.go
@@ -79,7 +79,53 @@ func TestDifference(t *testing.T) {
 }
 
 func TestSymmetricDifference(t *testing.T) {
+	arrOfA1 := []A{{1}, {2}, {3}}
+	arrOfA2 := []A{}
+	arrOfB1 := []B{{2}, {3}, {4}}
+	arrOfB2 := []B{}
+	arrOfB3 := []B{{3}, {4}, {5}}
+	arrOfB4 := []B{{4}, {5}, {6}}
 
+	expectSymmetricDifferenceA1B1A := []A{{1}}
+	expectSymmetricDifferenceA1B1B := []B{{4}}
+	expectSymmetricDifferenceA1B2A := []A{{1}, {2}, {3}}
+	expectSymmetricDifferenceA1B2B := []B{}
+	expectSymmetricDifferenceA2B1A := []A{}
+	expectSymmetricDifferenceA2B1B := []B{{2}, {3}, {4}}
+	expectSymmetricDifferenceA2B2A := []A{}
+	expectSymmetricDifferenceA2B2B := []B{}
+	expectSymmetricDifferenceA1B3A := []A{{1}, {2}}
+	expectSymmetricDifferenceA1B3B := []B{{4}, {5}}
+	expectSymmetricDifferenceA1B4A := []A{{1}, {2}, {3}}
+	expectSymmetricDifferenceA1B4B := []B{{4}, {5}, {6}}
+
+	equalAB := func(a *A, b *B) bool {
+		return a.va == b.vb
+	}
+
+	a, b := SymmetricDifference(arrOfA1, arrOfB1, equalAB)
+	assert.Equal(t, expectSymmetricDifferenceA1B1A, a)
+	assert.Equal(t, expectSymmetricDifferenceA1B1B, b)
+
+	a, b = SymmetricDifference(arrOfA1, arrOfB2, equalAB)
+	assert.Equal(t, expectSymmetricDifferenceA1B2A, a)
+	assert.Equal(t, expectSymmetricDifferenceA1B2B, b)
+
+	a, b = SymmetricDifference(arrOfA2, arrOfB1, equalAB)
+	assert.Equal(t, expectSymmetricDifferenceA2B1A, a)
+	assert.Equal(t, expectSymmetricDifferenceA2B1B, b)
+
+	a, b = SymmetricDifference(arrOfA2, arrOfB2, equalAB)
+	assert.Equal(t, expectSymmetricDifferenceA2B2A, a)
+	assert.Equal(t, expectSymmetricDifferenceA2B2B, b)
+
+	a, b = SymmetricDifference(arrOfA1, arrOfB3, equalAB)
+	assert.Equal(t, expectSymmetricDifferenceA1B3A, a)
+	assert.Equal(t, expectSymmetricDifferenceA1B3B, b)
+
+	a, b = SymmetricDifference(arrOfA1, arrOfB4, equalAB)
+	assert.Equal(t, expectSymmetricDifferenceA1B4A, a)
+	assert.Equal(t, expectSymmetricDifferenceA1B4B, b)
 }
 
 func TestSubset(t *testing.T) {
